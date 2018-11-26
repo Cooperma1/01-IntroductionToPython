4af1280a8b4e57cb987417cf3e19dd5411bc0e12
cooperma <cooperma@rose-hulman.edu>
2018-11-26 12:36:17 -0500

    DONE

--- src/m2_todo_and_commit_push.py
+++ src/m2_todo_and_commit_push.py
@@ -71,7 +71,7 @@ print('Hello, World')
 print('apples are red')
 ###############################################################################
 #
-# TODO: 4.
+# Done: 4.
 #   Add yet another   print   statement, putting it below this comment.
 #   This one should print the *product* of 3,607 and 34,227.
 #   Let the computer do the arithmetic for you (no calculators!).
@@ -86,7 +86,7 @@ print('apples are red')
 print(3607*34227)
 ###############################################################################
 #
-# TODO: 5.
+# Done: 5.
 #   Look at the list of files in the Project window (to the left).
 #   Note that this file:
 #      m2_todo_and_commit_push.py
